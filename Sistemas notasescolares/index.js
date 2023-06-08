a713da65ad5b65f24ff05867c6c07372ff61dc9e
Vitor Hugo <123006549+vitorhugo03@users.noreply.github.com>
2023-06-08 12:16:09 -0300

    Add files via upload

---
+++ Sistemas notasescolares/index.js
@@ -0,0 +1,24 @@
+//Sistemas de notas escolares//
+
+let score = 60
+let scoreA = score >=90 && score<=100
+let scoreB = score >=80 && score<=89
+let scoreC = score >=70 && score<=79
+let scoreD = score >=60 && score<=69
+let scoreF = score <60 && score>=0
+
+let scoreFinal;
+    if(scoreA){
+        scoreFinal ="A"
+    } else if(scoreB){
+        scoreFinal = "B"
+    }else if(scoreC){
+        scoreFinal ="C"
+    }else if(scoreD){
+        scoreFinal ="D"
+    }else if(scoreF){
+        scoreFinal="F"
+    }else{
+        scoreFinal = "Nota inválida"
+    }
+    console.log(scoreFinal)
